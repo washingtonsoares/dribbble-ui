3ae91af824142fdebb23a7e3c8dfdf9f593d44c5
Washington Soares <washingtonudi@gmail.com>
2017-06-28 14:46:06 -0300

    Removing unnecessary component

--- src/shots/shots.jsx
+++ src/shots/shots.jsx
@@ -1,25 +1,49 @@
 import React, {Component} from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import ShotMenu from '../shot-menu/shot-menu'
-import ShotList from './shotList'
+import InfiniteScroll from 'redux-infinite-scroll'
+
 import './shots.scss'
+import ShotMenu from '../shot-menu/shot-menu'
+import Shot from '../shot/shot'
 import {getShots} from './shotActions'
 
 class Shots extends Component {
+	constructor (props) {
+    super(props)
+  }
+
+  _loadMore () {
+    if (!this.props.isFetching) {
+      this.props.getShots()
+    }
+  }
+
+  _renderShots (shotList) {
+    return shotList.map(
+				(shot, i) => <Shot shot={shot} key={i} />
+			)
+  }
+
   render () {
     const {shotList} = this.props
     return (
       <div>
         <ShotMenu />
-        <ShotList shots={shotList} />
+				<InfiniteScroll
+	        className='shots'
+	        items={this._renderShots(shotList)}
+	        elementIsScrollable={false}
+	        loadMore={this._loadMore.bind(this)}
+	        />
       </div>
     )
   }
 }
 
 const mapStateToProps = state => ({
-  shotList: state.shot.shotList
+  shotList: state.shot.shotList,
+	isFetching: state.shot.isFetching
 })
 
 const mapDispatchToProps = dispatch => bindActionCreators({getShots}, dispatch)
