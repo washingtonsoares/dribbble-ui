98202a7af28bbf913a2cdc0aa758fcf5ce737fb1
Washington Soares <washingtonudi@gmail.com>
2017-06-28 15:17:53 -0300

    Adjusting CSS

--- src/shot/shot.jsx
+++ src/shot/shot.jsx
@@ -8,7 +8,7 @@ export default props => {
 	  <div className='shot'>
 	    <figure>
 	      <img src={shot.images.teaser} alt='An awesome picture' />
-	      <figcaption>Fig. {props.title}</figcaption>
+	      <figcaption>{shot.title}</figcaption>
 	    </figure>
 	    <div className='shot-author'>
 	      <img src={shot.user.avatar_url} />
