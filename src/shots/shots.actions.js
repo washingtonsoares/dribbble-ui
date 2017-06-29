c753ec9c9b9a49487d8ecb919127a40857c67ea3
Washington Soares <washingtonudi@gmail.com>
2017-06-29 00:41:07 -0300

    Writing ShotsActions test

--- src/shots/shots.actions.js
+++ src/shots/shots.actions.js
@@ -6,7 +6,7 @@ export const getShots = () => {
   return (dispatch, getState) => {
     let {currentPage} = getState().shot
     dispatch({ type: 'FETCH_SHOTS' })
-    axios.get(`${BASE_URL}/shots?page=${currentPage}&per_page=12&access_token=${ACCESS_TOKEN}`)
+    return axios.get(`${BASE_URL}/shots?page=${currentPage}&per_page=12&access_token=${ACCESS_TOKEN}`)
     .then(({data}) => {
       dispatch({ type: 'FETCH_SHOTS_SUCCESS', payload: data })
     })
---
+++ src/shots/shots.test.js
@@ -0,0 +1,40 @@
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import * as actions from './shots.actions'
+import axios from 'axios' // v0.15.3
+import httpAdapter from 'axios/lib/adapters/http'
+import nock from 'nock'
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
+const host = 'https://api.dribbble.com/v1'
+
+axios.defaults.host = host
+axios.defaults.adapter = httpAdapter
+
+describe('async actions', () => {
+  afterEach(() => {
+    nock.cleanAll()
+  })
+
+  it('creates FETCH_SHOTS_SUCCESS when fetching shots', () => {
+		nock(host)
+      .get(/shots/)
+      .reply(200, [{id: 1}] )
+
+    const expectedActions = [
+      { type: 'FETCH_SHOTS' },
+      { type: 'FETCH_SHOTS_SUCCESS', payload: [{id: 1}] }
+    ]
+
+    const store = mockStore({
+			 shot: { }
+		})
+
+    return store.dispatch(actions.getShots()).then(() => {
+      // return of async actions
+      expect(store.getActions()).toEqual(expectedActions)
+    })
+  })
+})
